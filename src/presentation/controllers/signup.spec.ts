c81bc05d2d3e71b68392e16253a6adddf225cfae
Atila Rampazo <atilarampazo@gmail.com>
2020-03-29 12:12:15 -0300

    refactor: make SignUpController conform to a protocol

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -45,7 +45,6 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
-
   test('Should return 400 if no password confirmation is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
