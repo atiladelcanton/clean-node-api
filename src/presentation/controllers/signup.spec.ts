8f62b3be9a8ec1f3a9786f1f10561301074936fc
Atila Rampazo <atilarampazo@gmail.com>
2020-03-30 07:42:13 -0300

    test: ensure SIgnUpController calls EmailValidator  with correct email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -99,4 +99,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
+
+  test('Should coll EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    const httpRequest = {
+      body: {
+        name: 'Atila',
+        email: 'any_email@email.com',
+        password: '111',
+        passwordConfirmation: '111'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com')
+  })
 })
